c0a8bf2ead0ce94c778e4a2b2d610c47e71d31ed
mazelina <mazelina@ukm.edu.my>
2015-10-26 10:43:45 +0800

    add kkk addd

---
+++ calculator.php
@@ -0,0 +1,18 @@
+<?php
+class Calculator{
+	function add(a,b){
+
+
+	$result=0;
+
+	function add(a,b){
+		$this->result = a + b;
+	}
+
+	function showResult(){
+		echo $this->result;
+	}
+
+		
+	}
+}
